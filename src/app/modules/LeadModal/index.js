8f6abb88179c305cbf722cb12c3919a6053cc8b9
AdeelAkram <adeelakram696@gmail.com>
2024-04-04 03:36:08 +0500

    work till doc tab

--- src/app/modules/LeadModal/index.js
+++ src/app/modules/LeadModal/index.js
@@ -1,7 +1,13 @@
-import { Modal, Flex } from 'antd';
+import { Modal, Flex, Select } from 'antd';
+import classNames from 'classnames';
 import { FireFilledIcon } from 'app/images/icons';
+import styles from './LeadModal.module.scss';
 
 function LeadModal({ show, handleClose }) {
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
   return (
     <Modal
       open={show}
@@ -11,8 +17,32 @@ function LeadModal({ show, handleClose }) {
       className="leadModal"
     >
       <Flex>
-        <div><FireFilledIcon /></div>
-        <div>Alpine Radiant Construction</div>
+        <Flex className={classNames(styles.logo, styles.marginRight)} align="center"><FireFilledIcon /></Flex>
+        <Flex className={classNames(styles.title, styles.marginRight)} align="center">
+          Alpine Radiant Construction
+        </Flex>
+        <Flex className={classNames(styles.stageDropdown, styles.marginRight, styles.marginTopNeg)} align="center" vertical>
+          <div className={styles.statusLabel}>Stage</div>
+          <Select
+            className="stageDropdown"
+            defaultValue="1"
+            style={{ width: 144 }}
+            onChange={handleChange}
+            options={[
+              { value: '1', label: 'Application out' },
+              { value: '2', label: 'Ready' },
+              { value: '3', label: 'Moved' },
+            ]}
+          />
+        </Flex>
+        <Flex className={classNames(styles.status, styles.marginRight, styles.marginTopNeg)} align="center" vertical>
+          <div className={styles.statusLabel}>Sequence Name</div>
+          <div className={styles.statusValue}>Welcome W/O Doc & W/O Application</div>
+        </Flex>
+        <Flex className={classNames(styles.status, styles.marginRight, styles.marginTopNeg)} align="center" vertical>
+          <div className={styles.statusLabel}>Current Step</div>
+          <div className={styles.statusValue}>6-12 Days Later</div>
+        </Flex>
       </Flex>
     </Modal>
   );
